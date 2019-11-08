e476cbd3f76b741822a66a57c0f3a7beead50cd4
88plug <19512127+88plug@users.noreply.github.com>
2019-11-08 20:15:41 +0100

    Update sentinel-one-line-installer.sh

--- sentinel-one-line-installer.sh
+++ sentinel-one-line-installer.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-cd ~
-
-sudo apt-get update
-sudo apt-get -y install python-virtualenv virtualenv
-#sudo locale-gen "en_US.UTF-8"
-
-#export LC_ALL="en_US.UTF-8" && export LC_CTYPE="en_US.UTF-8"
-git clone https://github.com/zocteam/sentinel.git sentinel
-cd sentinel
+#BrixCoin Sentinel Setup
+apt-get update ; apt-get -y install python-virtualenv virtualenv ; git clone https://github.com/cryptoandcoffee/sentinel sentinel ; cd sentinel
 virtualenv ./venv
 ./venv/bin/pip install -r requirements.txt
-
 crontab -l > mycron
-echo "* * * * * cd $(pwd) && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py >> sentinel.log 2>&1" >> mycron
+echo "* * * * * cd /root/.brix/sentinel && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py >> sentinel.log 2>&1" >> mycron
 crontab mycron
 rm mycron
